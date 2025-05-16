cc5c6ab85e99c115708b6c72949888832a8fe0a9
Bonjov Hima <bonjovhima@hotmail.de>
2025-05-17 00:40:43 +0200

    customized the button

--- assets/components/UI/CustomButton.js
+++ assets/components/UI/CustomButton.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import { Text, Pressable, StyleSheet } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import Colors from '../../../constants/colors';
 
-const CustomButton = ({ children, onPress }) => {
+function CustomButton({ children, onPress }) {
   return (
-    <Pressable onPress={onPress} style={({ pressed }) => [styles.button, pressed && styles.pressed]}>
-      <Text style={styles.text}>{children}</Text>
-    </Pressable>
+    <View style={styles.buttonOuter}>
+      <Pressable
+        onPress={onPress}
+        style={({ pressed }) => [styles.buttonInner, pressed && styles.pressed]}
+        android_ripple={{ color: Colors.primary600 }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
   );
-};
+}
 
 export default CustomButton;
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: '#ddb52f',
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
-    marginHorizontal: 8,
+  buttonOuter: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
   },
-  text: { color: '#4e0329', fontSize: 16, fontWeight: 'bold' },
-  pressed: { opacity: 0.75 },
+  buttonInner: {
+    backgroundColor: Colors.primary500,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: Colors.white,
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
 });
